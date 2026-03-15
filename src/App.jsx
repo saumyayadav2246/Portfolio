e04b724e550b3f7659c143b0c6981cf74baaffd5
saumyayadav2246 <yadav.saumya131254@marwadiuniversity.ac.in>
2026-03-15 16:53:18 +0530

    Fix router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter basename="/Portfolio">
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="contact" element={<Contact />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
